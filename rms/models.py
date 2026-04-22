008490631d0f17d1e0338d701ad506c3f43b365e
diya <shresthadiya59@gmail.com>
2026-04-22 15:38:14 +0545

    postgres integrate

--- rms/models.py
+++ rms/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):# , Food
+   name = models.CharField(max_length=50)
+   
+   def __str__(self):
+      return self.name
+
+class Menu(models.Model):
+   name = models.CharField(max_length=150) # Coke, Sprite, Fanta
+   category = models.ForeignKey(Category, on_delete=models.CASCADE)# NULL
+   price = models.IntegerField()
+   
+   def __str__(self):
+      return f"{self.name} - {self.price}"
