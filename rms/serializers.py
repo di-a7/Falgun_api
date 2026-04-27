1e5ee0132548491aefdde60740495fc7cca336a9
diya <shresthadiya59@gmail.com>
2026-04-27 15:05:01 +0545

    category get api

---
+++ rms/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+
+class CategorySerializer(serializers.Serializer):
+   id = serializers.IntegerField()
+   name = serializers.CharField()
+
+
+# table: view, url, serializer
--- rms/views.py
+++ rms/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Category
+from .serializers import CategorySerializer
 # Create your views here.
+
+@api_view()
+def category(request):
+   category = Category.objects.all()      # [{'id': '1', 'name':"hello"}]
+   serializer = CategorySerializer(category, many=True)                 # instance/queryset is converted into json format
+   return Response(serializer.data)
+
